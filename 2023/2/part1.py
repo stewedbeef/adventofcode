5917771264851328bf9bcb0aaa37cf22b9cc05e3
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2023-12-02 03:00:10 -0800

    Day 2, 2023

---
+++ 2023/2/part1.py
@@ -0,0 +1,23 @@
+def process(s: str) -> int:
+    game = s.split(":")
+    id = int(game[0][len("Game "):])
+
+    for roll in game[1].split(";"):
+        result = {"red": 0, "green": 0, "blue": 0}
+
+        for res in roll.split(","):
+            res1 = res.split()
+            result[res1[1]] += int(res1[0])
+
+        if (result["red"] > 12
+            or result["green"] > 13
+            or result["blue"] > 14):
+            return 0
+    return id
+
+i = 0
+with open("2023/2/data.txt", "r") as file:
+    for line in file:
+        i += process(line)
+
+print(i)
---
+++ 2023/2/part2.py
@@ -0,0 +1,20 @@
+def process(s: str) -> int:
+    rolls = s.split(":")[1].split(";")
+    result = {"red": 0, "green": 0, "blue": 0}
+    
+    for roll in rolls:
+        for res in roll.split(","):
+            res1 = res.split()
+
+            if (result[res1[1]] < int(res1[0])):
+                result[res1[1]] = int(res1[0])
+    return tuple(result.values())
+
+i = 0
+with open("2023/2/data.txt", "r") as file:
+    for line in file:
+        r, g, b = process(line)
+        print(r, g, b)
+        i += r * g * b
+
+print(i)
